119e48bf2ecf18c37ab2ff9a53ba69271280ca05
Chen <chen.jiang.1206@gmail.com>
2023-06-05 08:23:12 -0400

    done

--- server/app.py
+++ server/app.py
@@ -21,14 +21,14 @@ api = Api(app)
 class ClearSession(Resource):
 
     def delete(self):
-    
+
         session['page_views'] = None
         session['user_id'] = None
 
         return {}, 204
 
 class IndexArticle(Resource):
-    
+
     def get(self):
         articles = [article.to_dict() for article in Article.query.all()]
         return articles, 200
@@ -48,9 +48,38 @@ class ShowArticle(Resource):
 
         return {'message': 'Maximum pageview limit reached'}, 401
 
+class Login(Resource):
+
+    def post(self):
+        username = request.json['username']
+        user = User.query.filter_by(username=username).first()
+        if user:
+            session['user_id'] = user.id
+            return user.to_dict(), 200
+        else:
+            return {'message': 'user not found'}, 400
+
+class Logout(Resource):
+
+    def delete(self):
+        session['user_id'] = None
+        return {}, 204
+
+class CheckSession(Resource):
+
+    def get(self):
+        user_id = session['user_id']
+        if user_id:
+            user = User.query.filter_by(id=user_id).first()
+            return user.to_dict(), 200
+        return {}, 401
+
 api.add_resource(ClearSession, '/clear')
 api.add_resource(IndexArticle, '/articles')
 api.add_resource(ShowArticle, '/articles/<int:id>')
+api.add_resource(Login, '/login')
+api.add_resource(Logout, '/logout')
+api.add_resource(CheckSession, '/check_session')
 
 
 if __name__ == '__main__':
